6d6fecd7d96c176bbd44e6ad072b3db4b3eaf62d
ikbalrazu <iqbalraju123@gmail.com>
2023-06-14 17:14:00 +0600

    getstaticpaths

--- pages/getstaticpaths/index.js
+++ pages/getstaticpaths/index.js
@@ -1,10 +1,28 @@
 
-function index() {
+function Postlist({data}) {
+    console.log(data);
     return (
         <div>
-            Enter
+            {data.map((post,index)=>{
+                return(
+                    <div key={index}>
+                        <h1>{post.title}</h1>
+                        <p>{post.body}</p>
+                    </div>
+                )
+            })}
         </div>
     );
 }
 
-export default index;
+export async function getStaticProps(ctx){
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    return{
+        props:{
+            data: data,
+        }
+    }
+}
+
+export default Postlist;
